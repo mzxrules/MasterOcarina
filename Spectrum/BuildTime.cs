9164c9eddbf4c33d73842eb73d054976db0729e8
mzxrules <mzxrules@gmail.com>
2019-04-01 02:03:45 -0400

    rename ActorSpawn files

--- Spectrum/BuildTime.cs
+++ Spectrum/BuildTime.cs
@@ -1,1 +1,1 @@
-namespace Spectrum { partial class Program { public const string Timestamp = "Mon 04/01/2019  1:49:07.41";}} 
+namespace Spectrum { partial class Program { public const string Timestamp = "Mon 04/01/2019  2:02:51.57";}} 
